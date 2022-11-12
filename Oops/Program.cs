7002f7ab526b8af11fe994561268b3d3c784875c
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-12 22:29:41 +0530

    [Lalit] Add: Folder Structure Created

---
+++ Oops/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Oops
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Oops Problem Statements");
+        }
+    }
+}
